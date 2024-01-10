409a174fae95ee7aa5a48a9b6e734971d16b87fe
pujak <pk8184732@gmail.com>
2024-01-10 14:23:51 +0530

    orientation

--- meadia_quiry_flutter/lib/main.dart
+++ meadia_quiry_flutter/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page' ),
     );
   }
 }
